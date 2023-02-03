52df792cf53c5d550618f399730c22d779993aca
verstands <rabbymbamu@gmail.com>
2023-02-03 08:46:40 +0100

    agent controller

--- serveur.js
+++ serveur.js
@@ -4,10 +4,14 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000
 
+
+const routerAdmin = require('./routes/adminsitrateurRoute')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended : true}))
 
+
+app.use('/api', routerAdmin)
 app.get('/', (req, res) => {
     res.json({message : 'Api'})
 })
