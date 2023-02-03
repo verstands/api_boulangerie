0b1080a9004e5c8d2d9fa4e4e3c989162b0a656b
verstands <rabbymbamu@gmail.com>
2023-02-03 08:35:49 +0100

    controllerAdmin

---
+++ controllers/administrateurController.js
@@ -0,0 +1,61 @@
+const db  = require('../models')
+const ADMIN = db.administrateur
+
+const addAdmin = async (req, res) => {
+    let data = {
+        nom : req.nom.body,
+        postnom : req.postnom.body,
+        email : req.email.body,
+        adresse : req.adresse.body,
+        password : req.password.body
+    }
+
+    const admin = ADMIN.create({ data })
+    let message = "l'administrateur a ete crée avec success";
+    res.status(200).json({ 
+        message : message,
+        data : admin
+    });
+}
+
+const getAll = async (req, res) => {
+    const all = ADMIN.findAll({})
+    let message = "Les administrateur";
+    req.status(200).json({
+        message : message,
+        data : all
+    });
+}
+
+const getfindId = async (req, res) => {
+    let id = req.params.id;
+    const getId = AMDIN.findOne({where : {id : id}})
+    res.status(200).json({
+        data : getId
+    })
+}
+
+const updateId = async (res, req) => {
+    let id = req.params.id
+    const updatId = ADMIN.update({where : {id : id}})
+    let message = "La modification a ete fait avec success"
+    res.status(200).json({
+        message : message
+    })
+}
+const deleteId = async (req, res) => {
+    let id = req.params.id
+    const DeleteId = ADMIN.delete({ where : {id : id}})
+    let message = "La suppresion a ete fait avec success"
+    req.status(200).json({
+        message : message
+    })
+}
+
+module.exports = {
+    addAdmin,
+    getAll,
+    getfindId,
+    deleteId,
+    updateId
+}
