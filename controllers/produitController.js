06ec5fe84646aa5fefb0c0b316c637477a8302b0
verstands <rabbymbamu@gmail.com>
2023-02-03 09:42:41 +0100

    produit controller

---
+++ controllers/produitController.js
@@ -0,0 +1,57 @@
+const db  = require('../models')
+const ADMIN = db.fonction
+
+const addAdmin = async (req, res) => {
+    let data = {
+        nomCFonc : req.nomCFonc.body,
+    }
+
+    const admin = await ADMIN.create({ data })
+    let message = "la fonction a ete crée avec success";
+    res.status(200).json({ 
+        message : message,
+        data : admin
+    });
+}
+
+const getAll = async (req, res) => {
+    const all = await ADMIN.findAll({})
+    let message = "Les fonctions";
+    res.status(200).json({
+        message : message,
+        data : all
+    });
+}
+
+const getfindId = async (req, res) => {
+    let id = req.params.id;
+    const getId = await ADMIN.findOne({where : {id : id}})
+    res.status(200).json({
+        data : getId
+    })
+}
+
+const updateId = async (res, req) => {
+    let id = req.params.id
+    const updatId = await ADMIN.update({where : {id : id}})
+    let message = "La modification a ete fait avec success"
+    res.status(200).json({
+        message : message
+    })
+}
+const deleteId = async (req, res) => {
+    let id = req.params.id
+    const DeleteId = await ADMIN.destroy({ where : {id : id}})
+    let message = "La suppresion a ete fait avec success"
+    res.status(200).json({
+        message : message
+    })
+}
+
+module.exports = {
+    addAdmin,
+    getAll,
+    getfindId,
+    deleteId,
+    updateId
+}
